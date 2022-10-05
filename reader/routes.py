dddcf146b70ddfa57f01937c1ea8e014ef712ba8
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-10-05 09:38:28 +0500

    Пагинация

--- reader/routes.py
+++ reader/routes.py
@@ -1,12 +1,12 @@
-from flask import render_template, send_from_directory, request
-
 from reader import app
 from reader.models import Book
+from flask import render_template, send_from_directory, request
 
 
 @app.route('/')
 def index():
-    books = Book.query.all()
+    page = request.args.get('page', 1, type=int)
+    books = Book.query.order_by(Book.created_at.desc()).paginate(page=page, per_page=4)
     return render_template('index.html', books=books)
 
 
@@ -19,6 +19,20 @@ def send_file(filename):
 def book(book_id):
     book = Book.query.get_or_404(book_id)
     return render_template('book.html', book=book)
+
+
+@app.route('/thrillers/')
+def thrillers():
+    page = request.args.get('page', 1, type=int)
+    books = Book.query.filter(Book.genre == 'триллер').paginate(page=page, per_page=4)
+    return render_template('thrillers.html', books=books)
+
+
+@app.route('/best/')
+def best():
+    page = request.args.get('page', 1, type=int)
+    books = Book.query.filter(Book.rating > 4).paginate(page=page, per_page=4)
+    return render_template('best.html', books=books)
 
 
 @app.route('/thrillers/')
