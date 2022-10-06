f2b5f51c354568973ea4a22f04e0dc8be5cadbdc
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-10-06 09:31:44 +0500

    Шаблоны для форм.

--- reader/forms.py
+++ reader/forms.py
@@ -19,3 +19,14 @@ class BookForm(FlaskForm):
         title = Book.query.filter_by(title=title.data).first()
         if title:
             raise ValidationError('Такая книга уже есть в списке прочитанных.')
+
+
+class UpdateBook(FlaskForm):
+    title = StringField('Название', validators=[DataRequired(), Length(min=5, max=100)])
+    author = StringField('Автор', validators=[DataRequired(), Length(min=5, max=100)])
+    genre = StringField('Жанр', validators=[DataRequired(), Length(min=5, max=20)])
+    cover = FileField('Обложка книги', validators=[FileAllowed(['jpg', 'png'])])
+    rating = IntegerField('Моя оценка', validators=[DataRequired(), NumberRange(min=1, max=5)])
+    description = TextAreaField('Сюжет', validators=[DataRequired(), Length(max=500)])
+    notes = TextAreaField('Заметки', validators=[DataRequired(), Length(max=500)])
+    submit = SubmitField('Обновить')
